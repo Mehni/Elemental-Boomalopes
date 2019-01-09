22e756b6dd0e4d4d884323696418fc92e1d91035
Alias44 <25497382+Alias44@users.noreply.github.com>
2019-01-09 16:08:30 -0700

    v1.0 update
    -Offloaded alope Base/Kind parent to the core Boomalope. This way alopes will always have the same values as core without reworking the code.  Additionally, a mod could make changes to all alopes without necessary needing to be aware of Elemental-Boomalopes.
    -Lowered spawn chances to be cumulatively equal to the core Boomalope (as technically all alopes breeds are competing for the same resources)
    -Moved repeated radius calculation to its own function
    -Removed old GenExplosion's
    -Fixed texture naming conventions

--- Source/Elemental Boomalopes/Elemental Boomalopes/DeathActionWorker.cs
+++ Source/Elemental Boomalopes/Elemental Boomalopes/DeathActionWorker.cs
@@ -8,6 +8,29 @@ using Verse;
 
 namespace Elemental_Boomalopes
 {
+	public class Alope
+	{
+		public static float DeathRadius (int lifeStage)
+		{
+			float radius;
+
+			if (lifeStage == 0)
+			{
+				radius = 1.9f;
+			}
+			else if (lifeStage == 1)
+			{
+				radius = 2.9f;
+			}
+			else
+			{
+				radius = 4.9f;
+			}
+
+			return radius;
+		}
+	}
+
     public class DeathActionWorker_BigExplosion_EMP : DeathActionWorker
     {
         public override RulePackDef DeathRules
@@ -19,22 +42,9 @@ namespace Elemental_Boomalopes
         }
         public override void PawnDied(Corpse corpse)
         {
-            float radius;
-            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
-            {
-                radius = 1.9f;
-            }
-            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
-            {
-                radius = 2.9f;
-            }
-            else
-            {
-                radius = 4.9f;
-            }
-            //GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.EMP, corpse.InnerPawn,     null, null, null, null, 0f, 1, false, null, 0f, 1);
-            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.EMP, corpse.InnerPawn, -1, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
-        }
+			float radius = Alope.DeathRadius(corpse.InnerPawn.ageTracker.CurLifeStageIndex);
+			GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.EMP, corpse.InnerPawn, -1, -1, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
+		}
     }
 
     //A17
@@ -78,68 +88,29 @@ namespace Elemental_Boomalopes
     {
         public override void PawnDied(Corpse corpse)
         {
-            float radius;
-            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
-            {
-                radius = 1.9f;
-            }
-            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
-            {
-                radius = 2.9f;
-            }
-            else
-            {
-                radius = 4.9f;
-            }
-            //GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Stun, corpse.InnerPawn,      null, null, null, null, 0f, 1, false, null, 0f, 1);
-            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Stun, corpse.InnerPawn, -1, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
-        }
+            float radius = Alope.DeathRadius(corpse.InnerPawn.ageTracker.CurLifeStageIndex);
+			GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Stun, corpse.InnerPawn, -1, -1, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
+		}
     }
 
     public class DeathActionWorker_BigExplosion_FireFoam : DeathActionWorker
     {
         public override void PawnDied(Corpse corpse)
         {
-            float radius;
-            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
-            {
-                radius = 3.8f;
-            }
-            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
-            {
-                radius = 5.8f;
-            }
-            else
-            {
-                radius = 9.8f;
-            }
-            ThingDef filthFireFoam = ThingDefOf.FilthFireFoam;
-            //GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Extinguish, corpse.InnerPawn, null, null, null, filthFireFoam, 1f, 2, true, null, 0f, 1);
-            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Extinguish, corpse.InnerPawn, -1, null, null, null, filthFireFoam, 1f, 2, true, null, 0f, 1, 0f, false);
-        }
+            float radius = Alope.DeathRadius(corpse.InnerPawn.ageTracker.CurLifeStageIndex);
+			ThingDef filthFireFoam = ThingDefOf.Filth_FireFoam;
+ 			GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Extinguish, corpse.InnerPawn, -1, -1, null, null, null, null, filthFireFoam, 1f, 2, true, null, 0f, 1, 0f, false);
+		}
     }
 
     public class DeathActionWorker_BigExplosion_Smoke : DeathActionWorker
     {
         public override void PawnDied(Corpse corpse)
         {
-            float radius;
-            if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0)
-            {
-                radius = 3.8f;
-            }
-            else if (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 1)
-            {
-                radius = 5.8f;
-            }
-            else
-            {
-                radius = 9.8f;
-            }
+            float radius= Alope.DeathRadius(corpse.InnerPawn.ageTracker.CurLifeStageIndex);
             ThingDef gas_Smoke = ThingDefOf.Gas_Smoke;
-            //GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Smoke, corpse.InnerPawn, null, null, null, gas_Smoke, 1f, 1, false, null, 0f, 1);
-            GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Smoke, corpse.InnerPawn, -1, null, null, null, gas_Smoke, 1f, 1, false, null, 0f, 1, 0f, false);
-        }
+			GenExplosion.DoExplosion(corpse.Position, corpse.Map, radius, DamageDefOf.Smoke, corpse.InnerPawn, -1, -1, null, null, null, null, gas_Smoke, 1f, 1, false, null, 0f, 1, 0f, false);
+		}
     }
 
 
